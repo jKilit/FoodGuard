def8f517d52e746e00ba5685f0a9f8237bfb9f04
edvinmujanovic <edvin.mujanovic@gmail.com>
2024-02-13 20:15:44 +0100

    Update contentview so it tests the API call

--- FoodGuard/ContentView.swift
+++ FoodGuard/ContentView.swift
@@ -9,58 +9,27 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
+    @State private var API = FoodAPI()
+    
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-#if os(macOS)
-            .navigationSplitViewColumnWidth(min: 180, ideal: 200)
-#endif
-            .toolbar {
-#if os(iOS)
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-#endif
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
+            VStack {
+                if let foodModel = API.foodModel {
+                    Text("Product name: \(foodModel.name)")
+                } else {
+                    Text("Loading...")
                 }
             }
-        } detail: {
-            Text("Select an item")
+            /*.task {
+                do {
+                    try await API.loadFood()
+                } catch {
+                    print("Error loading food: \(error)")
+                }
+            }*/
         }
     }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
-    }
-}
-
+// Use Preview instead of #Preview
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
 }
