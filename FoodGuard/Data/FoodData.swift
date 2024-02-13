35e047bf28ab1ece388f96d6183d951fa38aaad3
jonathankilit <Jonathan.kilit@hotmail.com>
2024-02-13 19:09:50 +0100

    add datafile for api

---
+++ FoodGuard/Data/FoodData.swift
@@ -0,0 +1,20 @@
+//
+//  FoodData.swift
+//  FoodGuard
+//
+//  Created by Jonathan Kilit on 2024-02-13.
+//
+
+import Foundation
+struct FoodData: Decodable {
+    let code: String
+    let product: Product
+    let status: Int
+    let statusVerbose: String
+    
+    struct Product: Decodable {
+        let ingredientsHierarchy: [String]
+        let productName: String
+    }
+}
+
