157ae8b8f74e470ced99f42fd1caeaeb4f04fa2e
edvinmujanovic <edvin.mujanovic@gmail.com>
2024-02-13 23:44:39 +0100

    Fix UserDefaults to save the value on Preferences and design the view

--- FoodGuard/Model/PreferencesModel.swift
+++ FoodGuard/Model/PreferencesModel.swift
@@ -9,14 +9,31 @@ import Foundation
 import Observation
 
 @Observable
-class PreferencesModel {
+class PreferencesModel: ObservableObject {
     var selectedIngredients: Set<String> = []
+
+    init() {
+        loadPreferences()
+    }
 
     func toggleIngredient(_ ingredient: String) {
         if selectedIngredients.contains(ingredient) {
             selectedIngredients.remove(ingredient)
         } else {
             selectedIngredients.insert(ingredient)
+        }
+        savePreferences()
+    }
+
+    
+    //maybe fix enum for the values
+    private func savePreferences() {
+        UserDefaults.standard.set(Array(selectedIngredients), forKey: "selectedIngredients")
+    }
+
+    private func loadPreferences() {
+        if let savedIngredients = UserDefaults.standard.array(forKey: "selectedIngredients") as? [String] {
+            selectedIngredients = Set(savedIngredients)
         }
     }
 }
---
+++ FoodGuard/Views/PreferencesView.swift
@@ -0,0 +1,40 @@
+//
+//  PreferencesView.swift
+//  FoodGuard
+//
+//  Created by Edvin Mujanovic on 2024-02-13.
+//
+
+import SwiftUI
+
+struct PreferencesView: View {
+    @State var preferencesModel: PreferencesModel
+    
+    let ingredients = ["Lactose", "Eggs", "Nuts", "Gluten", "Soy", "Fish"]
+    
+    var body: some View {
+        Image("1")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(height: 200)
+            .padding()
+        
+        Text("Preferences:")
+            .font(.title)
+            .fontWeight(.bold)
+            .padding(.bottom, 10)
+        
+        
+        List {
+            ForEach(ingredients, id: \.self) { ingredient in
+                Toggle(isOn: Binding(
+                    get: { preferencesModel.selectedIngredients.contains(ingredient) },
+                    set: { _ in preferencesModel.toggleIngredient(ingredient) }
+                )) {
+                    Text(ingredient)
+                }
+            }
+        }
+        .navigationTitle("Preferences")
+    }
+}
