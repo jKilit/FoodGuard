18cb63971b605060edcd26015e7c1375a75001e6
edinnasic <edin.nasic27@gmail.com>
2024-02-20 11:50:17 +0100

    e-additives and allergies

--- FoodGuard/BarcodeScanner/Views/BarcodeScannerView.swift
+++ FoodGuard/BarcodeScanner/Views/BarcodeScannerView.swift
@@ -9,77 +9,67 @@ import Foundation
 import SwiftUI
 import SwiftUI
 
-enum Tab {
-    case home, screen2, scanner, screen4, screen5
-}
-
 struct BarcodeScannerView: View {
     @State private var API = FoodAPI()
     @State private var isFoodLoaded = false
-    
-    @State private var selectedTab: Tab = .scanner
     @State var viewModel = BarcodeScannerViewModel()
     
     var body: some View {
         ZStack(alignment: .bottom) {
-            // Camera Screen
             ScannerView(scannedCode: $viewModel.scannedCode, alertItem: $viewModel.alertItem)
                 .edgesIgnoringSafeArea(.all)
             
             // Black overlay
-            Color.black.opacity(0.7)
+            Color.black.opacity(0.4)
                 .edgesIgnoringSafeArea(.all)
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
             
-            // Overlay
             VStack {
-                Image(systemName: "barcode.viewfinder")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 200, height: 200)
+                
+                Text("Place the barcode inside the frame to scan.")
+                    .font(.subheadline)
                     .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
                     .padding()
                 
-                // Text/Barcode Display
-                VStack {
-                    Spacer()
-                    
-                    Text("Place the barcode inside the frame to scan.")
-                        .font(.subheadline)
-                        .foregroundColor(.white)
-                        .multilineTextAlignment(.center)
-                        .padding()
-                    
-                    
-                    Text("Scanned Barcode:")
-                        .font(.headline)
+                Spacer(minLength: 200)
+                
+                VStack{
+                    Image(systemName: "barcode.viewfinder")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 200, height: 200)
                         .foregroundColor(.white)
                         .padding()
                     
-                    if isFoodLoaded {
-                        // Display product name when food is loaded
-                        Text("Product name: \(API.foodModel?.name ?? "N/A")")
-                            .font(.title)
-                            .fontWeight(.bold)
-                            .foregroundColor(.white)
-                            .padding()
-                    } else {
-                        // Display scanned barcode
-                        Text("Scanned Barcode:")
-                            .font(.headline)
-                            .foregroundColor(.white)
-                            .padding(.top, 10)
-                        
-                        Text(viewModel.statusText)
-                            .font(.title)
-                            .fontWeight(.bold)
-                            .foregroundColor(.white)
-                            .padding()
-                    }
-                }
-                .frame(maxWidth: .infinity)
-            }
             
+                    VStack {
+                        Spacer()
+                        if isFoodLoaded {
+                            // Display product name when food is loaded
+                            Text("Product name: \(API.foodModel?.name ?? "N/A")")
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .foregroundColor(.white)
+                                .padding()
+                        } else {
+                            // Display scanned barcode
+                            Text("Scanned Barcode:")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .padding(.top, 10)
+                            
+                            Text(viewModel.statusText)
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .foregroundColor(.white)
+                                .padding()
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+                
+            }
             
         }
         .onChange(of: viewModel.scannedCode) { newScannedCode in
