37586aac983c02511693020ca5cdd0ddb15b7699
jonathankilit <Jonathan.kilit@hotmail.com>
2024-02-26 18:52:11 +0100

    Change logic with triggered ingredients (Edvin)

--- FoodGuard/Model/ProductModel.swift
+++ FoodGuard/Model/ProductModel.swift
@@ -4,35 +4,45 @@ import Observation
 @Observable
 class ProductModel {
     let preferencesModel = PreferencesModel()
-    var matchedProducts: [String] = []
-    
+    var matchedProducts: [(productName: String, alertTriggeringIngredients: Set<String>)] = []
+
     func compareIngredients(_ otherIngredients: [String], ingredientTags: [String], productName: String) -> Bool {
         let cleanedOtherIngredients = otherIngredients.map { $0.replacingOccurrences(of: "en:", with: "") }
-        
-        let intersection = Set(preferencesModel.selectedIngredients).intersection(cleanedOtherIngredients)
-        
+
         let isVegan = isProductVegan(ingredientTags: ingredientTags)
         let isPalmOilFree = isPalmOilFree(ingredientTags: ingredientTags)
         let palmoilInPreference = preferencesModel.selectedIngredients.contains("palm oil")
         let veganInPreference = preferencesModel.selectedIngredients.contains("animal products")
-        
+
+        var alertTriggeringIngredients: Set<String> = []
+
         if veganInPreference && !isVegan {
-            return false
+            alertTriggeringIngredients.insert("Non-Vegan")
         }
-        if palmoilInPreference && !isPalmOilFree{
-            return false
+
+        if palmoilInPreference && !isPalmOilFree {
+            alertTriggeringIngredients.insert("Contains Palm Oil")
         }
-        else if !intersection.isEmpty {
+
+        let intersection = Set(preferencesModel.selectedIngredients).intersection(cleanedOtherIngredients)
+
+        // Include all triggering ingredients, both from preferences and due to non-vegan or palm oil content
+        alertTriggeringIngredients.formUnion(intersection)
+
+        if !alertTriggeringIngredients.isEmpty {
+            // If there are alert-triggering ingredients, add the product name and ingredients to the matchedProducts array
+            matchedProducts.append((productName: productName, alertTriggeringIngredients: alertTriggeringIngredients))
             return false
         }
 
         return true
     }
-    
+
+
     func isProductVegan(ingredientTags: [String]) -> Bool {
         return !ingredientTags.contains("en:non-vegan")
     }
-    
+
     func isPalmOilFree(ingredientTags: [String]) -> Bool {
         return !ingredientTags.contains("en:palm-oil")
     }
--- FoodGuard/Views/ProductView.swift
+++ FoodGuard/Views/ProductView.swift
@@ -1,72 +1,69 @@
 import SwiftUI
 
-
 struct ProductView: View {
     @State private var preferencesModel = PreferencesModel()
     @State private var productModel = ProductModel()
-
+    
     let productName: String
     let ingredients: [String]
     let ingredientsTags: [String]
-
+    
     var body: some View {
         let isProductSafe: Bool = productModel.compareIngredients(ingredients, ingredientTags: ingredientsTags, productName: productName)
+        
         VStack {
-            let preferences = Array(preferencesModel.selectedIngredients)
-
             Text(productName)
                 .font(.title)
                 .foregroundColor(isProductSafe ? .green : .red)
                 .fontWeight(.bold)
                 .padding()
-
+            
             if isProductSafe {
                 // Green background for safe products
                 Text("This product is safe!")
                     .foregroundColor(.green)
                     .padding()
-
+                
                 // Checkmark image
                 Image(systemName: "checkmark.circle.fill")
                     .resizable()
                     .frame(width: 50, height: 50)
                     .foregroundColor(.green)
             } else {
-                // Red background for products with ingredients not in preferences
-                Text("This product contains ingredients not in your preferences!")
+                Text("This product does not match")
+                    .foregroundColor(.red)
+                Text("your preferences!")
                     .foregroundColor(.red)
                     .padding()
-
+                
                 // Red cross image
                 Image(systemName: "xmark.circle.fill")
                     .resizable()
                     .frame(width: 50, height: 50)
                     .foregroundColor(.red)
-            }
-
-            // Form with preferences
-            List {
-                Section(header: Text("Preferences (Ingredients)").font(.title3).foregroundColor(isProductSafe ? .green : .red)) {
-                    ForEach(preferences, id: \.self) { ingredient in
-                        Text(ingredient)
+                // Display matched products
+                
+                List {
+                    Section(header: Text("Alert-triggering ingredients:").font(.title3).foregroundColor(.orange)) {
+                        ForEach(productModel.matchedProducts, id: \.productName) { matchedProduct in
+                            VStack(alignment: .leading) {
+                                
+                                // Display alert-triggering ingredients
+                                ForEach(matchedProduct.alertTriggeringIngredients.sorted(), id: \.self) { alertTriggeringIngredient in
+                                    Text(alertTriggeringIngredient)
+                                        .padding(.vertical, 4)
+                                }
+                                
+                            }
                             .padding(.vertical, 8)
+                        }
                     }
                 }
-
-                Section(header: Text("Ingredients (for testing)").font(.title3).foregroundColor(isProductSafe ? .green : .red)) {
-                    ForEach(ingredients, id: \.self) { ingredient in
-                        // Remove "en:" prefix from each ingredient
-                        let cleanedIngredient = ingredient.replacingOccurrences(of: "en:", with: "")
-
-                        Text(cleanedIngredient)
-                            .padding(.vertical, 8)
-                    }
-                }
+                .listStyle(GroupedListStyle())
+                .frame(height: UIScreen.main.bounds.height / 2) // Half of the screen height
+                .cornerRadius(16)
+                .shadow(radius: 5)
             }
-            .listStyle(GroupedListStyle())
-            .frame(height: UIScreen.main.bounds.height / 2) // Half of the screen height
-            .cornerRadius(16)
-            .shadow(radius: 5)
         }
         .cornerRadius(16)
         .shadow(radius: 5)
