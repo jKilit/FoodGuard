49fad588eb3eda20494f5eebfa31d7b74fde3714
edvinmujanovic <edvin.mujanovic@gmail.com>
2024-03-04 12:08:45 +0100

    Make the entire preferenceview scrollable

--- FoodGuard/Views/PreferencesView.swift
+++ FoodGuard/Views/PreferencesView.swift
@@ -5,36 +5,41 @@
 //  Created by Edvin Mujanovic on 2024-02-13.
 //
 
+
 import SwiftUI
 
 struct PreferencesView: View {
     @State var preferencesModel: PreferencesModel
-
     
     let ingredients = ["lactose", "sugar", "eggs", "nuts","gluten", "soy", "fish", "animal products", "palm oil", "E-additives"]
+    
     var body: some View {
-        Image("1")
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .frame(height: 200)
-            .padding()
+        ScrollView {
+            VStack {
+                Image("1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 200)
+                    .padding(.bottom, 10)
                 
-        Text("I don't want to consume:")
-            .font(.title)
-            .fontWeight(.bold)
-            .padding(.bottom, 10)
-        
-        
-        List {
-            ForEach(ingredients, id: \.self) { ingredient in //is off by default fix this
-                Toggle(isOn: Binding(
-                    get: { preferencesModel.selectedIngredients.contains(ingredient) },
-                    set: { _ in preferencesModel.toggleIngredient(ingredient) }
-                )) {
-                    Text(ingredient)
+                Text("I don't want to consume:")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding(.bottom, 10)
+                
+                LazyVStack(alignment: .leading, spacing: 10) {
+                    ForEach(ingredients, id: \.self) { ingredient in
+                        Toggle(isOn: Binding(
+                            get: { preferencesModel.selectedIngredients.contains(ingredient) },
+                            set: { _ in preferencesModel.toggleIngredient(ingredient) }
+                        )) {
+                            Text(ingredient)
+                        }
+                    }
                 }
+                .navigationTitle("Preferences")
+                .padding()
             }
         }
-        .navigationTitle("Preferences")
     }
 }
